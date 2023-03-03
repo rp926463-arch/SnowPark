a435d6d19256acb17ea2b058cc133ecfb1cbca62
rp926463 <roshanpatil1703@gmail.com>
2023-03-03 10:38:23 +0530

    scala samples added

---
+++ Snowpark-Labs/scala_samples.scala
@@ -0,0 +1,301 @@
+To parse a fixed width file in Snowpark Scala, you can use the read method of the DataFrameReader class with the option method to specify the schema and format of the file.
+
+Here is an example code snippet to read a fixed width file with a specific schema:
+
+
+import io.projectglow.sql.DataFrameReader
+
+val spark = SparkSession.builder()
+  .appName("Fixed Width File Parser")
+  .getOrCreate()
+
+val schema = "col1 STRING(10), col2 INT, col3 DOUBLE"
+
+val df = DataFrameReader(spark)
+  .option("header", "true")
+  .option("format", "fixedwidth")
+  .option("schema", schema)
+  .option("widths", "10,5,10")
+  .load("/path/to/fixed/width/file")
+
+df.show()
+
+
+In this example, the schema variable specifies the schema of the file, where each column is defined by its name, data type, and width. The option("widths", "10,5,10") method specifies the width of each column in the file. The load method is used to read the file and create a DataFrame.
+
+You can modify the schema, widths, and other options as per your specific requirements. Once you have the DataFrame, you can perform further transformations and analysis on the data.
+======================================================================================================================
+
+val df = DataFrameReader(spark)
+  .option("header", "true")
+  .option("format", "fixedwidth")
+  .option("schema", schema)
+  .option("widths", "10,5,10")
+  .option("url", "s3://my-bucket/my-file.txt")
+  .option("awsAccessKeyId", "<AWS_ACCESS_KEY>")
+  .option("awsSecretKey", "<AWS_SECRET_KEY>")
+  .option("stageInfo", "stage_name@my_aws_stage")
+  .load()
+  
+===================================================================================================================
+ To parse a fixed width file in a Snowflake stage in Snowpark Scala dynamically, you can use the fs API to read the file from the stage and then use the SparkSession API to infer the schema and create a DataFrame.
+
+Here is an example code snippet to read a fixed width file from a Snowflake stage and infer the schema dynamically:
+
+import io.projectglow.sql.DataFrameReader
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types._
+
+val spark = SparkSession.builder()
+  .appName("Fixed Width File Parser")
+  .getOrCreate()
+
+val stageName = "my_stage"
+val fileName = "my_file.txt"
+
+// read the file from the stage
+val fileContent = spark.read
+  .format("snowflake")
+  .options(Map(
+    "sfURL" -> "<your_snowflake_account_url>",
+    "sfUser" -> "<your_snowflake_username>",
+    "sfPassword" -> "<your_snowflake_password>",
+    "sfDatabase" -> "<your_snowflake_database>",
+    "sfSchema" -> "<your_snowflake_schema>",
+    "sfWarehouse" -> "<your_snowflake_warehouse>"
+  ))
+  .option("query", s"SELECT $fileName FROM @$stageName")
+  .load()
+
+// infer the schema dynamically
+val widths = Seq(10, 5, 10) // specify the width of each column
+val fields = for (i <- 0 until widths.length) yield {
+  StructField(s"col$i", StringType)
+}
+val schema = StructType(fields)
+
+val df = DataFrameReader(spark)
+  .option("header", "false")
+  .option("format", "fixedwidth")
+  .option("schema", schema.toString())
+  .option("widths", widths.mkString(","))
+  .load(fileContent.as[String])
+
+df.show()
+
+
+In this example, the fileContent DataFrame is created by reading the file from the Snowflake stage using the query option. The widths variable specifies the width of each column in the file. The fields and schema variables are used to dynamically infer the schema of the file. Finally, the DataFrameReader is used to create a DataFrame with the inferred schema and the specified column widths.
+
+Note that this example assumes that the file has a fixed width format, and that each column is a string type. You may need to modify the code based on the specific requirements of your file. Also, make sure to replace the Snowflake connection options with your own values.
+
+======================================================================================================================
+
+import io.projectglow.sql.DataFrame
+import io.projectglow.sql.functions._
+
+val spark = SparkSessionProvider.session
+
+// Create a DataFrame with some sample data
+val df = DataFrame(Seq(
+  ("John", "Doe"),
+  ("Jane", "Smith"),
+  ("Bob", "Johnson")
+)).toDF("first_name", "last_name")
+
+// Define a lambda function to extract the first two characters of the first name
+val extractFirstName: (String => String) = (firstName: String) => firstName.substring(0, 2)
+
+// Apply the lambda function using withColumn
+val df2 = df.withColumn("short_name", udf(extractFirstName).apply(col("first_name")))
+
+// Show the output
+df2.show()
+
+
+==============================================================================================================
+
+import io.projectglow.sql.DataFrame
+import io.projectglow.sql.expressions._
+import io.projectglow.sql.functions._
+
+val df = DataFrame(Seq(
+  ("John", "Doe", "123 Main St."),
+  ("Jane", "Smith", "456 Oak St."),
+  ("Bob", "Johnson", "789 Maple St.")
+)).toDF("first_name", "last_name", "address")
+
+// Define a lambda function to apply substring on a column
+val substringFunc = (colName: String) => {
+  df.withColumn(colName, substring(col(colName), 1, 3))
+}
+
+// Apply the substring function on all columns using foldLeft
+val df2 = df.columns.foldLeft(df) { (acc, colName) =>
+  substringFunc(colName)
+}
+
+df2.show()
+
+
+In this example, the df variable is a DataFrame with three columns: first_name, last_name, and address. The substringFunc lambda function takes a column name as input and applies the substring function on that column. The foldLeft method is used to apply the substringFunc function on each column in the df DataFrame.
+
+Note that this example assumes that you want to apply the substring function on all columns with the same parameters (substring(col(colName), 1, 3)). You can modify the lambda function and the foldLeft method to apply different substring functions on each column with different parameters.
+
+==============================================================================================================
+
+import io.projectglow.sql.DataFrame
+import io.projectglow.sql.expressions._
+import io.projectglow.sql.functions._
+
+val df = DataFrame(Seq(
+  ("John", "Doe", "123 Main St."),
+  ("Jane", "Smith", "456 Oak Ave."),
+  ("Bob", "Johnson", "789 Elm St.")
+)).toDF("first_name", "last_name", "address")
+
+val substrFuncs = Seq(
+  (col: Column) => substring(col, 1, 3), // apply substring from 1st to 3rd character
+  (col: Column) => substring(col, 2, 4), // apply substring from 2nd to 4th character
+  (col: Column) => substring(col, 4, 6) // apply substring from 4th to 6th character
+)
+
+val df2 = df.mapColumns { (colName, colExpr) =>
+  // get the index of the current column in the DataFrame
+  val colIndex = df.columns.indexOf(colName)
+  
+  // apply the corresponding substring function to the column
+  substrFuncs(colIndex % substrFuncs.length)(colExpr)
+}
+
+df2.show()
+
+In this example, the df DataFrame is created with some sample data. The substrFuncs variable is a sequence of lambda functions that apply different substring functions on each column. The mapColumns method is used to apply these functions on each column of the DataFrame.
+
+The colIndex variable gets the index of the current column in the DataFrame. The modulo operation % substrFuncs.length is used to cycle through the substrFuncs sequence for each column. The corresponding substring function is applied to the column expression.
+
+The resulting DataFrame df2 has the same number of columns as df, with each column having a different substring applied to it.
+
+Note that this example assumes that you have already created a Snowpark DataFrame df with the necessary columns. You may need to modify the code based on your specific requirements.
+
+==============================================================================================================
+
+
+import io.projectglow.sql.DataFrame
+import io.projectglow.sql.functions._
+import io.projectglow.sql.SparkSessionProvider
+
+val spark = SparkSessionProvider.session
+
+val df = DataFrame(Seq(
+  ("abcd", "efgh", "ijkl"),
+  ("mnop", "qrst", "uvwx"),
+  ("yz12", "3456", "7890")
+)).toDF("col1", "col2", "col3")
+
+// Define substring functions for each column
+val substringFunctions = Seq(
+  (col: String) => substring(col, 1, 2), // First two characters
+  (col: String) => substring(col, 2, 3), // Characters 2-4
+  (col: String) => substring(col, 3, 4)  // Last four characters
+)
+
+// Define column positions and names
+val columnPositions = Seq(
+  ("col1", 0),
+  ("col2", 1),
+  ("col3", 2)
+)
+
+// Define the lambda function to apply substring on all columns
+val substringLambda = (df: DataFrame) => {
+  val columns = columnPositions.map { case (name, pos) =>
+    // Get the column position and apply the corresponding substring function
+    val substringFunc = substringFunctions(pos)
+    substringFunc(col(s"_${name}"))
+      .as(name)
+  }
+  df.select(columns: _*)
+}
+
+// Apply the lambda function to the DataFrame
+val df2 = substringLambda(df)
+
+// Show the results
+df2.show()
+
+
+
+In this example, we define substringFunctions as a sequence of functions, where each function takes a column as input and applies a substring operation with specific parameters. We also define columnPositions as a sequence of tuples that map each column name to its position in the input JSON string.
+
+The substringLambda function takes a DataFrame as input, and applies the corresponding substring function for each column, based on its position in the input JSON string. We use the map method to iterate over the columnPositions sequence and apply the corresponding substring function for each column. We then use the select method to create a new DataFrame with the modified columns.
+
+Finally, we apply the substringLambda function to the input DataFrame to create df2, which is the output with the substring applied to each column. Note that we use the _ character to prefix each column name in the input DataFrame, so we need to remove this prefix when selecting the column name in the substringLambda function. Also, you can modify the substringFunctions, columnPositions, and substringLambda functions based on your specific requirements.
+
+=============================================================================================================
+
+import io.projectglow.functions._
+
+// define the JSON string with column names and positions
+val jsonString =
+"""
+{
+  "properties": [
+    {
+      "name": "col1",
+      "position": "1,2"
+    },
+    {
+      "name": "col2",
+      "position": "3,6"
+    }
+  ]
+}
+"""
+
+// parse the JSON string and create a list of (name, position) tuples
+val namePositions = io.circe.parser
+  .decode[Map[String, Seq[Map[String, String]]]](jsonString)
+  .toOption
+  .flatMap(_.get("properties"))
+  .map(_.map(p => (p("name"), p("position").split(",").map(_.toInt))))
+  .getOrElse(Seq.empty)
+
+// define the UDF to apply the substring function on each column
+val substringUdf = (col: Column, position: Array[Int]) => {
+  substring(col, position(0), position(1) - position(0) + 1)
+}
+
+// apply the UDF on each column with its position
+val df2 = df.mapColumns { case (col, name) =>
+  namePositions.find(_._1 == name) match {
+    case Some((_, pos)) => substringUdf(col, pos)
+    case None => col
+  }
+}
+
+// show the resulting DataFrame
+df2.show()
+
+
+In this example, the JSON string with column names and positions is parsed and converted into a list of (name, position) tuples. The substringUdf UDF is defined to apply the substring function with the specified parameters on each column. Finally, the mapColumns method is used to apply the UDF on each column with its name, using the namePositions list to find the corresponding position for each column.
+
+Note that this example assumes that the Snowpark DataFrame is already created and that the substring function is imported from the io.projectglow.functions package. You may need to modify the code based on your specific requirements.
+
+
+
+last question asked:
+
+use substring on all columns of snowpark dataframe in lambda function scala, apply different substring functions on each column with different parameters & accept column position in Json string with column names.
+
+like 
+{
+"properties" : [{
+"name":"col1",
+"position":"1,2"
+},
+{
+"name":"col2",
+"position":"3,6"
+}
+]
+}
